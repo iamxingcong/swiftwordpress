a6176146598d7754e0b01e3761ed02cfc3b8afd6
iamxingcong <121950263@qq.com>
2024-09-24 20:33:22 +0400

    Initial Commit

---
+++ Wordprs/WordprsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordprsApp.swift
+//  Wordprs
+//
+//  Created by c x on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct WordprsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
